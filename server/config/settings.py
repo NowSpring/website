060df421bd5431f548b80cc8e432b1da3ed556ca
NowSpring <kodai218@icloud.com>
2024-02-18 17:51:37 +0900

    add library

--- server/config/settings.py
+++ server/config/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Library
+    'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
+    # Installed app
 ]
 
 MIDDLEWARE = [
