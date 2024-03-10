21266dcbfbc3f8a8306d379e77aff72d2d136782
NowSpring <kodai218@icloud.com>
2024-03-10 18:53:02 +0900

    install vuetify + mdi

--- client/mysite/src/main.ts
+++ client/mysite/src/main.ts
@@ -2,10 +2,26 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useRouter } from './route'
 
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import './style.css'
 import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi', 
+  },
+})
 
 createApp(App)
   .use(createPinia())
   .use(useRouter)
+  .use(vuetify)
   .mount('#app')
