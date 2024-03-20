ef83ea5662c496f56bf669408cc67c8725f611a7
NowSpring <kodai218@icloud.com>
2024-03-20 14:58:37 +0900

    install unplugin-vue-components

--- client/mysite/vite.config.ts
+++ client/mysite/vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components(),
+    AutoImport({
+      imports: ['vue'],
+    }),
     // tsconfigPaths(),
   ],
   resolve: {
