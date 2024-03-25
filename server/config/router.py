8eea6063871e98bd8e2172c49f5df8f74013ea55
NowSpring <kodai218@icloud.com>
2024-03-25 23:42:39 +0900

    add review's api path

--- server/config/router.py
+++ server/config/router.py
@@ -2,9 +2,13 @@ from rest_framework import routers
 
 from apps.members.views import MemberViewSet
 from apps.comics.views import ComicMasterViewSet, ComicVersionViewSet, ComicEpisodeViewSet
+from apps.reviews.views import ReviewMasterViewSet, ReviewVersionViewSet, ReviewEpisodeViewSet
 
 router = routers.DefaultRouter()
 router.register('member', MemberViewSet)
 router.register('comic/master', ComicMasterViewSet)
 router.register('comic/version', ComicVersionViewSet)
 router.register('comic/episode', ComicEpisodeViewSet)
+router.register('review/master', ReviewMasterViewSet)
+router.register('review/version', ReviewVersionViewSet)
+router.register('review/episode', ReviewEpisodeViewSet)
